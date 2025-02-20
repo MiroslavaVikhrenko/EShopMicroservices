4ba619664ed30ff8b8ec188a4590bb76d286d6d5
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-02-19 18:41:11 -0700

    Add two interfaces - returning response and not returning response

--- src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -2,7 +2,11 @@
 
 namespace BuildingBlocks.CQRS
 {
-    public interface ICommand<out TResponse> : IRequest<TResponse>
+    public interface ICommand : ICommand<Unit> // Empty Command - does NOT return any response
+    {
+
+    }
+    public interface ICommand<out TResponse> : IRequest<TResponse> // Producing response (TResponse)
     {
     }
 }
