cf447fe94802ab13b32aa0e95aac8f0ed1950aab
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-02-19 18:18:52 -0700

    Add IRequest<> and IRequestHandler<> interfaces from MediatR

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,9 +1,17 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
 {
-    // command and result objects
-    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+    // Command and result objects
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        : IRequest<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandler
+    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
     {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            // Business logic to create a product
+            throw new NotImplementedException();
+        }
     }
 }
