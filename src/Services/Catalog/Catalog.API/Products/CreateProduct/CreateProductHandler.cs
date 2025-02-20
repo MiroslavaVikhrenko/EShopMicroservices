a6a7f60874a1c5011f5a04423f36a971dfa4c86d
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-02-19 19:10:50 -0700

    Refactor CreateProductCommandHandler class using abstraction on MediatR for CQRS

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,14 +1,16 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using MediatR;
 
 namespace Catalog.API.Products.CreateProduct
 {
     // Command and result objects
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
-        : IRequest<CreateProductResult>;
+        : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler 
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
             // Business logic to create a product
             throw new NotImplementedException();
