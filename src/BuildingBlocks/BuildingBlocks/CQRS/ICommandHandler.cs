d4930cbb8566441d7b918d64ee9b075adb08da3a
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-02-19 18:54:49 -0700

    Create abstraction ICommandHandler interfaces

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    public interface ICommandHandler<in TCommand> // Does NOT return any response
+        : ICommandHandler<TCommand, Unit>
+        where TCommand : ICommand<Unit>
+    {
+
+    }
+
+    public interface ICommandHandler<in TCommand, TResponse> // Producing response (TResponse)
+        : IRequestHandler<TCommand, TResponse>
+        where TCommand : ICommand<TResponse>
+        where TResponse : notnull
+    {
+    }
+}
