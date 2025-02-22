1294a87054c05550b6e5ad1378469df55dc321d9
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-02-22 16:57:41 -0700

    Define request and response records for endpoint

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,5 +1,8 @@
 ﻿namespace Catalog.API.Products.CreateProduct
 {
+    // Define request and response records for endpoint
+    public record CreateProductRequest(string Name, List<string> Categories, string Description, string ImageFile, decimal Price);
+    public record CreateProductResponse(Guid Id);
     public class CreateProductEndpoint
     {
     }
