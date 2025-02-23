b2687fcbbeac2de7ca72a2f37fed09dbfb3c30f7
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-02-22 18:49:58 -0700

    Register MediatR and Carter libraries into Asp.Net Dependency Injection Service

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+    // Register Carter library which will handle minimal API endpoints into ASP.NET DI container
+    // Carter provides a structured way to define routes using Carter model
+builder.Services.AddCarter();
+    // Register MidiatR library which will manage command and query handlers
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+}); // adding required services into MediatR =>
+// tells MediatR where to find command and query handler classes
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
+    // Configure HTTP request pipeline with Carter in order to expose HTTP get/post methods
+    // Carter maps routes defined in ICarterModule implementation
+app.MapCarter();
 
 app.Run();
