fdfc48041fc036e42e51ee2657030a2826b07464
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-02-22 17:07:44 -0700

    Implement ICarterModule

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,9 +1,15 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using Carter;
+
+namespace Catalog.API.Products.CreateProduct
 {
     // Define request and response records for endpoint
     public record CreateProductRequest(string Name, List<string> Categories, string Description, string ImageFile, decimal Price);
     public record CreateProductResponse(Guid Id);
-    public class CreateProductEndpoint
+    public class CreateProductEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
