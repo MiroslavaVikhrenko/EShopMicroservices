d50830015e046f226c6fb55f1d667eda32500895
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-02-22 19:48:11 -0700

    Register and Configure Marten DocumentDB library into Program.cs ASP.NET DI

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// STEP 1. Add services to the container.
     // Register Carter library which will handle minimal API endpoints into ASP.NET DI container
     // Carter provides a structured way to define routes using Carter model
 builder.Services.AddCarter();
@@ -9,11 +9,18 @@ builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
 }); // adding required services into MediatR =>
-// tells MediatR where to find command and query handler classes
+    // tells MediatR where to find command and query handler classes
+
+// Register and configure Marten DocumentDB library into ASP.NET DI (for PostgreSQL)
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!); // temp placeholder
+}).UseLightweightSessions();
+//customize Marten sessions using session operations - UseLightweightSessions() = best practices for performance
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline
+// STEP 2. Configure the HTTP request pipeline
     // Configure HTTP request pipeline with Carter in order to expose HTTP get/post methods
     // Carter maps routes defined in ICarterModule implementation
 app.MapCarter();
